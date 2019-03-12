55e55d62185af8e4434b1412b958edaff6ec7960
Jake Welton <jwelton122@googlemail.com>
2019-03-12 08:30:34 +0000

    Overwrite existing files

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -9,7 +9,7 @@ cd $dir
 # Create symlinks
 for file in $files; do
   echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/$file
+  ln -s -f $dir/$file ~/$file
   echo "- - -"
 done
 
