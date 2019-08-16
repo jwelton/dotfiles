5580ee85b18721c3c6b420a4b4085612dfe44077
Jake Welton <jwelton122@googlemail.com>
2019-08-16 21:35:35 +0100

    Add custom XCode theme

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 sh ~/.dotfiles/setup-brew.sh
 sh ~/.dotfiles/setup-symlinks.sh
+
+# Setup custom Xcode theme. Credit goes to: https://github.com/hdoria/xcode-themes
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+cp Space\ Gray.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
