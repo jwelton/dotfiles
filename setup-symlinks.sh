b2bc8ae0276aa3cf3c5ab7ef1d19297fb5ce442f
Jake Welton <jwelton122@googlemail.com>
2019-08-17 11:37:50 +0100

    Use symlink for XCode theme

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -10,8 +10,14 @@ cd $dir
 for file in $files; do
   echo "Creating symlink to $file in home directory."
   ln -s -f $dir/$file ~/$file
-  echo "- - -"
 done
 
 # initialize new settings
+echo "Initializing new bash settings"
 source ~/.bash_profile
+
+
+# Setup custom Xcode theme. Credit goes to: https://github.com/hdoria/xcode-themes
+echo "Creating symlink for XCode theme."
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+ln -s -f ./Space\ Gray.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
